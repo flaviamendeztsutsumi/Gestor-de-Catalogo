be37bc3c976cef5a6e36c517be3e3f4bfbb8a0e6
flaviamendeztsutsumi <145556069+flaviamendeztsutsumi@users.noreply.github.com>
2023-12-12 10:14:43 +0100

    Delete aplicacion directory

--- aplicacion/Principal.java
+++
@@ -1,9 +0,0 @@
-package aplicacion;
-import presentacion.*;
-public class Principal {
-
-    public static void main(String[] args) {
-        Interfaz interfaz = new Interfaz();
-        interfaz.iniciar();
-    }
-}
